6253b8d6cfe024c2f0c8b153fa79c5f2da2dc4a8
Ruslan <volna475@mail.ru>
2023-06-03 15:51:05 +0300

    Добавил вывод стоимости всех инструментов в метод GET account

--- src/accounts/router.py
+++ src/accounts/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, insert, update, delete
+from sqlalchemy import select, insert, update, delete, func, case, or_
 import json
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_session
-from models import account, instrument
+from models import account, instrument, user, total_quantity_and_avg_price_instrument_account, currency_type, instrument_type
 from accounts.schemas import Account, UpdateAccount
 
 router = APIRouter(
@@ -13,30 +13,88 @@ router = APIRouter(
 )
 
 
+# @router.get("/")
+# async def get_accounts(id: int ,session: AsyncSession = Depends(get_async_session)):
+#     try:
+#         query = select(account).where(account.c.user_id == id)
+#         result = await session.execute(query)
+#         accounts_by_user = [{row[0]: {
+#             'account_name': row[1], 
+#             'broker_name': row[2], 
+#             'date': row[3], 
+#             'user_id': row[4], 
+#             }
+#             } for row in result.all()]
+#         return {"accounts_by_user": accounts_by_user}
+#         # accounts = result.fetchall()
+#         # json_str = json.dumps(accounts, default=str)
+#         # json_dict = json.loads(json_str)
+#         # return json.dumps(json_dict, ensure_ascii=False)
+#     except Exception:
+#         # Передать ошибку разработчикам
+#         raise HTTPException(status_code=500, detail={
+#             "status": "error",
+#             "data": None,
+#             "details": "User don't have account"
+#         })
+
+
 @router.get("/")
-async def get_accounts(id: int ,session: AsyncSession = Depends(get_async_session)):
+async def get_accounts(id: int, session: AsyncSession = Depends(get_async_session)):
     try:
         query = select(account).where(account.c.user_id == id)
         result = await session.execute(query)
-        accounts_by_user = [{row[0]: {
-            'account_name': row[1], 
-            'broker_name': row[2], 
-            'date': row[3], 
-            'user_id': row[4], 
+        accounts_by_user = [
+            {
+                row[0]: {
+                    'account_name': row[1],
+                    'broker_name': row[2],
+                    'date': row[3],
+                    'user_id': row[4],
+                    'total_volume_by_account_in_RUB': await get_total_volume_in_RUB(row[0], session)
+                }
             }
-            } for row in result.all()]
+            for row in result.all()
+        ]
         return {"accounts_by_user": accounts_by_user}
-        # accounts = result.fetchall()
-        # json_str = json.dumps(accounts, default=str)
-        # json_dict = json.loads(json_str)
-        # return json.dumps(json_dict, ensure_ascii=False)
     except Exception:
-        # Передать ошибку разработчикам
         raise HTTPException(status_code=500, detail={
             "status": "error",
             "data": None,
-            "details": "User don't have account"
+            "details": "User doesn't have any accounts"
         })
+
+async def get_total_volume_in_RUB(account_id: int, session: AsyncSession):
+    try:
+        query = (
+            select(
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                )
+            )
+            .select_from(total_quantity_and_avg_price_instrument_account)
+            .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
+            .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
+            .where(account.c.id == account_id)
+        )
+        result = await session.execute(query)
+        total_volume_in_RUB = result.scalar()
+        return total_volume_in_RUB
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={
+            "status": "error",
+            "data": None,
+            "details": str(e)
+        })
+
+
+
     
 @router.post("/")
 async def add_specific_operations(new_account: Account, session: AsyncSession = Depends(get_async_session)):
--- src/instruments/router_volume.py
+++ src/instruments/router_volume.py
@@ -342,20 +342,16 @@ async def get_percent_currency_on_all_instruments(account_id: int, session=Depen
                     .select_from(
                         total_quantity_and_avg_price_instrument_account
                         .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
-                        #.join(user, user.c.id == account.c.user_id)
                         .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
                     )
-                    #.where(user.c.id == user_id)
                     .where(account.c.id == account_id)
                 ).label('share')
             )
             .select_from(
                 total_quantity_and_avg_price_instrument_account
                 .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
-                #.join(user, user.c.id == account.c.user_id)
                 .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
             )
-            #.where(user.c.id == user_id)
             .where(account.c.id == account_id)
             .group_by(currency_type.c.carrency_name)
         )
@@ -426,11 +422,6 @@ async def get_percent_currency_on_all_instruments(user_id: int, session=Depends(
             .group_by(currency_type.c.carrency_name)
         )
         result = await session.execute(query)
-        # percent_currency_on_all_instruments = result.fetchall()
-        # json_str = json.dumps(percent_currency_on_all_instruments, default=str, ensure_ascii=False)
-        # json_dict = json.loads(json_str)
-        # return json.dumps(json_dict, ensure_ascii=False)
-
         percent_currency_on_all_instruments = [{str(row[0]): row[1]} for row in result.all()]
         return {"percent_currency_on_all_instruments": percent_currency_on_all_instruments}
 
@@ -441,3 +432,129 @@ async def get_percent_currency_on_all_instruments(user_id: int, session=Depends(
             "data": None,
             "details": str(e)
         })
+
+# Получение доли компаний для счёта, для круговой диаграммы
+@router.get("/{account_id}/percent_of_companies_for_account", description="Получение доли компаний для счёта, для круговой диаграммы")
+async def get_percent_of_companies_by_account(account_id: int, session=Depends(get_async_session)):
+    try:
+        subquery = (
+            select(
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                )
+            )
+            .select_from(total_quantity_and_avg_price_instrument_account
+                         .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
+                         .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
+                         )
+            .where(account.c.id == account_id)
+            .where(total_quantity_and_avg_price_instrument_account.c.instrument_type_id == 1)
+            .subquery()
+        )
+
+        query = (
+            select(
+                total_quantity_and_avg_price_instrument_account.c.instrument_name,
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                ).label('total_amount'),
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                ) * 100 / subquery
+            )
+            .select_from(total_quantity_and_avg_price_instrument_account
+                         .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
+                         #.join(user, user.c.id == account.c.user_id)
+                         .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
+                         )
+            .where(account.c.id == account_id)
+            .where(total_quantity_and_avg_price_instrument_account.c.instrument_type_id == 1)
+            .group_by(total_quantity_and_avg_price_instrument_account.c.instrument_name)
+        )
+
+        result = await session.execute(query)
+        percent_of_companies_for_account1 = [{row[0]: {'total_amount': row[1], 'share': row[2]}} for row in result.all()]
+        return {"percent_of_companies_for_account": percent_of_companies_for_account1}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"status": "error", "data": None, "details": str(e)})
+
+# Получение доли компаний для пользователя, для круговой диаграммы
+@router.get("/{user_id}/percent_of_companies_for_user", description="Получение доли компаний для пользователя, для круговой диаграммы")
+async def get_percent_of_companies_by_user_id(user_id: int, session=Depends(get_async_session)):
+    try:
+        subquery = (
+            select(
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                )
+            )
+            .select_from(total_quantity_and_avg_price_instrument_account
+                         .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
+                         .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
+                         )
+            .where(account.c.user_id == user_id)
+            .where(total_quantity_and_avg_price_instrument_account.c.instrument_type_id == 1)
+            .subquery()
+        )
+
+        query = (
+            select(
+                total_quantity_and_avg_price_instrument_account.c.instrument_name,
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                ).label('total_amount'),
+                func.sum(
+                    case(
+                        (
+                            total_quantity_and_avg_price_instrument_account.c.instrument_name.in_(["RUB", "EUR", "USD", "HKD", "CHY"]),
+                            total_quantity_and_avg_price_instrument_account.c.total_quantity * currency_type.c.rate
+                        ),
+                        else_=total_quantity_and_avg_price_instrument_account.c.total_quantity * total_quantity_and_avg_price_instrument_account.c.avg_price * currency_type.c.rate
+                    )
+                ) * 100 / subquery
+            )
+            .select_from(total_quantity_and_avg_price_instrument_account
+                         .join(account, account.c.id == total_quantity_and_avg_price_instrument_account.c.account_id)
+                         .join(user, user.c.id == account.c.user_id)
+                         .join(currency_type, currency_type.c.id == total_quantity_and_avg_price_instrument_account.c.currency_id)
+                         )
+            .where(user.c.id == user_id)
+            .where(total_quantity_and_avg_price_instrument_account.c.instrument_type_id == 1)
+            .group_by(total_quantity_and_avg_price_instrument_account.c.instrument_name)
+        )
+
+        result = await session.execute(query)
+        percent_of_companies_for_user1 = [{row[0]: {'total_amount': row[1], 'share': row[2]}} for row in result.all()]
+        return {"percent_of_companies_for_user": percent_of_companies_for_user1}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail={"status": "error", "data": None, "details": str(e)})
